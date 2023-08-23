9b1ceed77da4a432668543141b8a43db84378b9d
full-stack-david <davidchan12323@gmail.com>
2023-08-23 16:14:39 -0500

    Search and display users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment  } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
+import Users from "./components/Users";
 
 export class App extends Component {
   constructor(props) {
@@ -24,37 +26,55 @@ export class App extends Component {
   };
 
   searchUsers = async (query) => {
-    this.loading = true;
+    this.setState({
+      loading: true
+    });
 
     const res = await axios.get(`https://api.github.com/search/users?q=${query}`);
 
-    this.users = res.data?.items;
-    this.loading = false;
+    this.setState({
+      loading: false,
+      users: res.data?.items
+    });
   };
 
   render() {
     const {
-      alert
+      alert,
+      loading,
+      users
     } = this.state;
 
     return (
-      <div className="App">
-        <Navbar icon="fab fa-github" title="Github Finder" />
+      <Router>
+        <div className="App">
+          <Navbar icon="fab fa-github" title="Github Finder" />
 
-        <div className="mt-5 col-md-9" style={{ margin: "auto" }}>
-          {alert !== null && (
-            <div className={`alert alert-${alert.type}`}>
-              <i className="fas fa-info-circle" />{' '}
-              {alert.msg}
-            </div>
-          )}
+          <div className="mt-4 col-md-9" style={{ margin: "auto" }}>
+            {alert !== null && (
+              <div className={`alert alert-${alert.type}`}>
+                <i className="fas fa-info-circle" />{' '}
+                {alert.msg}
+              </div>
+            )}
 
-          <Search
-            setAlert={this.showAlert}
-            searchUsers={this.searchUsers}
-          />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <Fragment>
+                  <Search
+                    setAlert={this.showAlert}
+                    searchUsers={this.searchUsers}
+                  />
+
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
