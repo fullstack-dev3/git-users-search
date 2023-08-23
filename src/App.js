76a5f99a105f9d40a5b19d339966962355945d18
full-stack-david <davidchan12323@gmail.com>
2023-08-23 04:30:12 -0500

    Add Search section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alert: null,
+      loading: false,
+      users: [],
+    }
+  }
+
+  showAlert = (msg, type) => {
+    this.setState({
+      alert: { msg, type }
+    });
+
+    setTimeout(() => this.alert = null, 5000);
+  };
+
+  searchUsers = async (query) => {
+    this.loading = true;
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${query}`);
+
+    this.users = res.data?.items;
+    this.loading = false;
+  };
+
   render() {
+    const {
+      alert
+    } = this.state;
+
     return (
       <div className="App">
         <Navbar icon="fab fa-github" title="Github Finder" />
+
+        <div className="mt-5 col-md-9" style={{ margin: "auto" }}>
+          {alert !== null && (
+            <div className={`alert alert-${alert.type}`}>
+              <i className="fas fa-info-circle" />{' '}
+              {alert.msg}
+            </div>
+          )}
+
+          <Search
+            setAlert={this.showAlert}
+            searchUsers={this.searchUsers}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Search = ({ setAlert, searchUsers }) => {
+  const [text, setText] = useState('');
+ 
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if (text === "") {
+      setAlert("Please enter something", "secondary");
+    } else {
+      searchUsers(text);
+      setText('');
+    }
+  };
+
+  const onChange = e => {
+    setText(e.target.value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <input
+          type="text"
+          name="text"
+          className="form-control"
+          placeholder="Search users..."
+          value={text}
+          onChange={onChange}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary btn-block">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+Search.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  searchUsers: PropTypes.func.isRequired,
+};
+
+export default Search;
